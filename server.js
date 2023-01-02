4806a1d4573b2e514798cc887ced6425df79a5a7
Subrat Gupta <114558072+subrat-gupta@users.noreply.github.com>
2023-01-02 13:14:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ var express = require("express");
 var app = express();
 var path = require("path");
 var fs = require("fs");
-const { dirname, normalize } = require("path");
+
 
 //server configuration
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
-});
-app.get('/hello',(req,res)=> {
-    res.sendFile(path.join(__dirname,'public/index.html'));
-    
 });
 
 app.get('/home', function (req, res) {
